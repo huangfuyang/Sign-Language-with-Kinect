acd4d6e7c7f3f61b88069d4513426c9ad9c18622
nwoedf <ckchan.cs@gmail.com>
2014-11-06 18:46:50 +0800

    Read frames of video and construct callback

--- HandShapeDetector/src/train.py
+++ HandShapeDetector/src/train.py
@@ -1,6 +1,13 @@
-from os import listdir
+from os import listdir,getcwd
 from os.path import isfile, join
 import csv
+import cv2
+
+# Constants
+labelDirectory = getcwd()+'/../data/label'
+videoDirectory = getcwd()+'/../data/video'
+videoFilenameExtension = 'avi'
+DEBUG_MODE = True
 
 # Read config
 def readCSV(fileName):
@@ -8,12 +15,30 @@ def readCSV(fileName):
         return [tuple(line) for line in csv.reader(csvfile, delimiter=',', quotechar='\'')]        
 
 # Read AVI video
+def readVideo(fileName, frameCallback, labels):
+    cap = cv2.VideoCapture(fileName)
+    i = 0
+    
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if (not ret) | (i>=len(labels)) | (cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+        frameCallback(frame, labels[i])
+        i = i+1
+    cap.release()
+
 # Extract hand image from video
+def extractHand(frame, label):
+    if DEBUG_MODE:
+        print label
+        cv2.imshow('detection', frame)
+
 # Feature extraction using Caffe
 # Train SVM model
 
-labelDirectory = '../data/label'
 fileList = [ f for f in listdir(labelDirectory) if isfile(join(labelDirectory,f)) ]
 
 for fileName in fileList:
-    labels = readCSV(join(labelDirectory,fileName))
+    labels = readCSV(join(labelDirectory, fileName))
+    readVideo(join(videoDirectory,'depth_'+fileName[:-3]+videoFilenameExtension), extractHand, labels)
+    cv2.destroyAllWindows()
