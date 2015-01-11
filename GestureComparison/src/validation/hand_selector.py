069e51edeead039a012af6fd96df9a7274fafb8d
nwoedf <ckchan.cs@gmail.com>
2015-01-11 22:31:07 +0800

    Better data flow

--- GestureComparison/src/validation/hand_selector.py
+++ GestureComparison/src/validation/hand_selector.py
@@ -8,11 +8,12 @@ class HandSelector(AbstractProcessor):
         self.select_right = select_right
 
     def process(self, data):
+        raw_data = data['raw_data']
         if self.select_left and self.select_right:
-            return np.concatenate((data[:,56:59], data[:,63:66]))
+            data['hands'] = np.concatenate((raw_data[:,56:59], raw_data[:,63:66]))
         elif self.select_left:
-            return data[:,56:59]
+            data['hands'] = raw_data[:,56:59]
         elif self.select_right:
-            return data[:,63:66]
-        else:
-            return None
+            data['hands'] = raw_data[:,63:66]
+
+        return data
