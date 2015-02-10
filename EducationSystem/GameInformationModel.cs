1a01152d3e4aff5f817ce0cd8cc6b232e2cd3eeb
nwoedf <ckchan.cs@gmail.com>
2015-02-10 15:47:49 +0800

    Add game information model

---
+++ EducationSystem/GameInformationModel.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Xml;
+namespace EducationSystem
+{
+    class GameInformationModel
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+        }
+
+        private string _shortDescription;
+        public string ShortDescription
+        {
+            get { return _shortDescription; }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+        }
+
+        private string _gamePageLink;
+        public string GamePageLink
+        {
+            get { return _gamePageLink; }
+        }
+
+        public GameInformationModel(XmlNode source)
+        {
+            _title = source["Title"].InnerText;
+            _shortDescription = source["ShortDescription"].InnerText;
+            _description = source["Description"].InnerText;
+            _gamePageLink = source["GamePageLink"].InnerText;
+        }
+    }
+}
