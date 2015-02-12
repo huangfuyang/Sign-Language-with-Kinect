2cbc0536bffcbf4929674c4d17794411d0448e5b
nwoedf <ckchan.cs@gmail.com>
2015-02-13 03:39:49 +0800

    Add logic and models for sign number game

---
+++ EducationSystem/SignNumGame/Point.cs
@@ -0,0 +1,33 @@
+﻿
+namespace EducationSystem.SignNumGame
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Point Add(Point other)
+        {
+            return new Point(this.X + other.X, this.Y + other.Y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Point))
+            {
+                return false;
+            }
+            else
+            {
+                Point point = obj as Point;
+                return (this.X == point.X) && (this.Y == point.Y);
+            }
+        }
+    }
+}
---
+++ EducationSystem/SignNumGame/Tile.cs
@@ -0,0 +1,39 @@
+﻿
+namespace EducationSystem.SignNumGame
+{
+    public class Tile
+    {
+        public int Value { get; set; }
+
+        private Point _position;
+        public Point Position
+        {
+            get { return _position; }
+        }
+
+        private Point _previousPosition;
+        public Point PreviousPosition
+        {
+            get { return _previousPosition; }
+        }
+
+        public Tile MergeFrom { get; set; }
+
+
+        public Tile(int x, int y, int value)
+        {
+            this.Value = value;
+            this._position = new Point(x, y);
+            this._previousPosition = new Point(x, y);
+            this.MergeFrom = null;
+        }
+
+        public void UpdatePosition(Point newPosition)
+        {
+            _previousPosition.X = _position.X;
+            _previousPosition.Y = _position.Y;
+            _position.X = newPosition.X;
+            _position.Y = newPosition.Y;
+        }
+    }
+}
